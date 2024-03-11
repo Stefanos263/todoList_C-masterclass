7cf6e87f2a6cf7147a6e0d25184d0b74b2433c1c
Stefanos263 <stefanoskempas26@gmail.com>
2024-03-11 20:27:40 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,30 @@ Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
-string userChoice = Console.ReadLine();
-Console.WriteLine(userChoice);
+var userChoice = Console.ReadLine();
+
+if (userChoice == "S")
+{
+    PrintSelectedOption(" See all TODOS");
+}
+else if (userChoice == "A")
+{
+    PrintSelectedOption(" Add a TODO");
+}
+else if (userChoice == "R")
+{
+    PrintSelectedOption(" Remove a TODO");
+}
+else if(userChoice == "E")
+{
+    PrintSelectedOption(" Exit ");
+}
+
+
 
 Console.ReadKey();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected Option : " + selectedOption);
+}
